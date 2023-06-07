3a333218a6512a73fbb6f39873bc9ec3f783025c
ViktoriiaKN <113258083+ViktoriiaKN@users.noreply.github.com>
2023-06-07 21:20:41 +0200

    Statistics тa clsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import { Profile } from "./Profile/Profile";
 import user from "../data/user.json";
+import { Statistics } from "./Statistics/Statistics";
 
 export const App = () => {
   return (
     <div>
       <Profile avatar={user.avatar} location={user.location}
         tag={user.tag} stats={user.stats} username={user.username} />
+      <Statistics />
     </div>
   );
 };

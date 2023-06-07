17d66a40ec93a291b57435e14dd1eee37a8ff103
ViktoriiaKN <113258083+ViktoriiaKN@users.noreply.github.com>
2023-06-07 21:43:47 +0200

    Підключила всі 4 компоненти

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,11 @@
 import { Profile } from "./Profile/Profile";
 import user from "../data/user.json";
 import { Statistics } from "./Statistics/Statistics";
+import data from '../data/data.json';
+import { FriendList } from './FriendList/FriendList';
+import friends from '../data/friends.json';
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
+
 
 export const App = () => {
   return (
@@ -8,6 +13,8 @@ export const App = () => {
       <Profile avatar={user.avatar} location={user.location}
         tag={user.tag} stats={user.stats} username={user.username} />
       <Statistics />
+      <FriendList />
+      <TransactionHistory/>
     </div>
   );
 };

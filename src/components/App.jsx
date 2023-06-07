b5a5756fc1e5c093eafd5cf09a175aa498f2624c
ViktoriiaKN <113258083+ViktoriiaKN@users.noreply.github.com>
2023-06-07 20:40:55 +0200

    Profile jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import { Profile } from "./Profile/Profile";
+import user from "../data/user.json";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile avatar={user.avatar} location={user.location}
+        tag={user.tag} stats={user.stats} username={user.username} />
     </div>
   );
 };
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,29 @@
+export const Profile = ({username, tag, location, avatar, stats}) => {
+    return <><div className="profile">
+  <div className="description">
+    <img
+      src={avatar}
+      alt={tag}
+      className="avatar"
+    />
+            <p className="name">{ username}</p>
+            <p className="tag">@{ tag}</p>
+            <p className="location">{ location}</p>
+  </div>
+
+  <ul className="stats">
+    <li>
+                <span className="label">{ stats.followers}</span>
+      <span className="quantity">1000</span>
+    </li>
+    <li>
+                <span className="label">{ stats.views}</span>
+      <span className="quantity">2000</span>
+    </li>
+    <li>
+                <span className="label">{ stats.likes}</span>
+      <span className="quantity">3000</span>
+    </li>
+  </ul>
+</div></>;
+}

699de21879254d3482e23a722f45bd2879b4f1cd
ViktoriiaKN <113258083+ViktoriiaKN@users.noreply.github.com>
2023-06-08 17:32:16 +0200

    change size of Status styled

--- src/components/FriendListItem/FriendListItem.styled.js
+++ src/components/FriendListItem/FriendListItem.styled.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const Status = styled.span`
-  width: 10px;
-  height: 10px;
+  width: 15px;
+  height: 15px;
   border-radius: 50%;
   background-color: ${props => (props.status ? 'green' : 'red')};
   display: inline-block;

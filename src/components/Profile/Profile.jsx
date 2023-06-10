b4615a65635a7bbe1d44287f7952cce0af696679
ViktoriiaKN <113258083+ViktoriiaKN@users.noreply.github.com>
2023-06-10 11:16:11 +0200

    Створила компоненти стилізації Профайлу

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,31 +1,36 @@
 import PropTypes from 'prop-types';
+import { Container, Img, ContainerWrapper } from './Profile.styled';
 
 export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
     <>
-      <div className="profile">
-        <div className="description">
-          <img src={avatar} alt={tag} className="avatar" />
-          <p className="name">{username}</p>
-          <p className="tag">@{tag}</p>
-          <p className="location">{location}</p>
-        </div>
+      <ContainerWrapper>
+        <Container>
+          <div>
+            <Img />
+            <img src={avatar} alt={tag} width={50} />
 
-        <ul className="stats">
-          <li>
-            <span className="label">{stats.followers}</span>
-            <span className="quantity">1000</span>
-          </li>
-          <li>
-            <span className="label">{stats.views}</span>
-            <span className="quantity">2000</span>
-          </li>
-          <li>
-            <span className="label">{stats.likes}</span>
-            <span className="quantity">3000</span>
-          </li>
-        </ul>
-      </div>
+            <p className="name">{username}</p>
+            <p className="tag">@{tag}</p>
+            <p className="location">{location}</p>
+          </div>
+
+          <ul className="stats">
+            <li>
+              <span className="label">{stats.followers}</span>
+              <span className="quantity">1000</span>
+            </li>
+            <li>
+              <span className="label">{stats.views}</span>
+              <span className="quantity">2000</span>
+            </li>
+            <li>
+              <span className="label">{stats.likes}</span>
+              <span className="quantity">3000</span>
+            </li>
+          </ul>
+        </Container>
+      </ContainerWrapper>
     </>
   );
 };
---
+++ src/components/Profile/Profile.styled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const ContainerWrapper = styled.div`
+  width: 1200px;
+  display: flex;
+  justify-content: center;
+  background-color: aqua;
+`;
+
+export const Container = styled.div`
+  font-family: sans-serif;
+  font-size: 400;
+  height: 100%;
+  display: flex;
+  padding: 40px 80px;
+  flex-direction: column;
+  align-items: center;
+  border: 2px solid;
+`;
+
+export const Img = styled.img`
+  border-radius: 50%;
+`;
